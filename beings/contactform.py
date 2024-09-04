587c25cd9eba05e13e90e041de4661febcf50fd7
alexander98765 <alexandro3217@gmail.com>
2024-09-04 15:39:20 -0600

    add python

---
+++ beings/contactform.py
@@ -0,0 +1,9 @@
+from typing import Collection
+
+class Contactform:
+    def __init__(
+        self, name:str, email:str | None = None
+    ) -> None:
+        self.name = name
+        self.email = email
+        
---
+++ tests/test_contactform.py
@@ -0,0 +1,6 @@
+#from contactform import Contactform
+
+def test_init():
+    #contactform = Contactform("Alex", "alex@gmail.com")
+    #assert contactform.name == "Alex"
+    assert 8 == 8
